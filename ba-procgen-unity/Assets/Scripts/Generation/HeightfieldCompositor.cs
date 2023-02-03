a7a2749f0352ba615e972de721238ff1a5d8400f
Nicolas Kühn <kuehn.nicolas1999@gmail.com>
2023-02-03 02:36:05 +0100

    Added HeightfieldCompositor to manage octaves

---
+++ ba-procgen-unity/Assets/Scripts/Generation/HeightfieldCompositor.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace ProcGen.Generation
+{
+    public class HeightfieldCompositor
+    {
+        public List<Octave> Octaves { get; private set; }
+
+        public void AddOctave(Octave octave)
+        {
+            Octaves.Add(octave);
+        }
+
+        public void RemoveOctave(Octave octave)
+        {
+            Octaves.Remove(octave);
+        }
+
+        public float GetComposedHeight(int x, int z)
+        {
+            float composedHeight = 0.0f;
+
+            // Combine heights of the height generators of all octaves
+            foreach (Octave octave in Octaves)
+            {
+                composedHeight += octave.HeightfieldGenerator.GetHeight(x, z);
+            }
+
+            return composedHeight;
+        }
+
+        public HeightfieldCompositor()
+        {
+            // Initialize list of octaves (empty)
+            Octaves = new List<Octave>();
+        }
+    }
+}

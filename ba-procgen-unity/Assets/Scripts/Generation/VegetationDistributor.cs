ae422970bce3157f01cd7cff155c024cc4efc012
Nicolas Kühn <kuehn.nicolas1999@gmail.com>
2023-05-09 03:14:39 +0200

    Continued work on biome logic + refactoring (WIP)

--- ba-procgen-unity/Assets/Scripts/Generation/VegetationDistributor.cs
+++ ba-procgen-unity/Assets/Scripts/Generation/VegetationDistributor.cs
@@ -8,6 +8,12 @@ namespace ProcGen.Generation
     [ExecuteInEditMode]
     public class VegetationDistributor : MonoBehaviour
     {
+        // Constants for approximations of the standard normal integral
+        private const float BETA_1 = -0.0004406f;
+        private const float BETA_2 = 0.0418198f;
+        private const float BETA_3 = 0.9000000f;
+        private const float SQRT_PI = 1.7724539f;
+
         private const int MAX_PLANTS_IN_GRID_CELL = 10;
 
         // TODO: Add tree struct with (mesh, material, matrices) for multiple tree types
@@ -110,6 +116,20 @@ namespace ProcGen.Generation
         }
         
 
+        private float RunNormalDistributionRandomExperiment(float expVal, float stdDev)
+        {
+            // ... VERSION 1 (PAPER) ... //
+            //float z = (Random.value - expVal) / stdDev;
+            // z = Mathf.Clamp(z, -8.0f, 8.0f);        // See paper "A Sigmoid Approximation of the Standard Normal Integral"
+            // TODO: Use the INVERSE function of the original CDF below (currently NOT working!)
+            // return 1.0f / ( 1.0f + Mathf.Exp(-SQRT_PI * (BETA_1 * z*z*z*z*z + BETA_2 * z*z*z + BETA_3 * z) ) );
+
+            // ... VERSION 2 (OWN) ... //
+            float x = Random.value;
+            const float c_s = 1.6f;
+            return Mathf.Log(1.0f/x - 1.0f) * (stdDev/c_s) + expVal;
+        }
+
         // Iterate over terrain and add vegetation
         public void DistributeVegetationOnTerrain()
         {
@@ -163,28 +183,88 @@ namespace ProcGen.Generation
             float y = SettingsManager.Instance.HeightfieldCompositor.GetComposedHeight(centerX, centerZ);
 
             float PlantCount_ExpVal = Mathf.Lerp(0, MAX_PLANTS_IN_GRID_CELL, cellData.SoilFertility); // TODO: Add influence of height
-            float PlantCount_StdDev = Mathf.Exp( -24.0f * ((cellData.Climate - 0.5f) * (cellData.Climate - 0.5f)) ); // TODO: continue work
+            float PlantCount_StdDev = Mathf.Exp( -24.0f * ((cellData.Climate - 0.5f) * (cellData.Climate - 0.5f)) );
+
+            // Run random experiment with expected value and standard deviation to determine plant count
+            float plantCount = Mathf.RoundToInt(
+                Mathf.Clamp(
+                    RunNormalDistributionRandomExperiment(PlantCount_ExpVal, PlantCount_StdDev), 
+                    0.0f, 
+                    MAX_PLANTS_IN_GRID_CELL
+                )
+            );
+            // Debug.Log($"count: {plantCount} | exp: {PlantCount_ExpVal}, sd: {PlantCount_StdDev}");
 
 
-            // ... Plausability rules here ... //
+            // ... Determine spawn locations for plants using Poisson Disc Sampling ... //
+            // Create grid
+            const float r = 0.1f;
+            const int k = 30;
+            float w = r / Mathf.Sqrt(2);
 
+            int d = Mathf.FloorToInt(SettingsManager.Instance.ChunkSettings.gridCellSize / w);
+
+            int[,] grid = new int[d, d];
+
+            // Init grid
+            for (int z = 0; z < d; z++)
+            {
+                for (int x = 0; x < d; x++)
+                {
+                    grid[x, z] = -1;
+                }
+            }
+
+            // Select initial sample randomly
+            int sampleIndex = 0;
+            List<int> active = new List<int>();
+
+            float gridCellHalfSize = 0.5f * SettingsManager.Instance.ChunkSettings.gridCellSize;
+            float randomX = Random.Range(centerX - gridCellHalfSize, centerX + gridCellHalfSize);
+            float randomZ = Random.Range(centerZ - gridCellHalfSize, centerX + gridCellHalfSize);
+
+            int iX = Mathf.FloorToInt(randomX / w);
+            int iZ = Mathf.FloorToInt(randomZ / w);
+
+            grid[iX, iZ] = sampleIndex;
+            active.Add(sampleIndex);
+            sampleIndex++;
+
+            // Choose random sample from active list while not empty
+            while (active.Count > 0)
+            {
+                int randomIndex = Random.Range(0, active.Count);
+
+                for (int i = 0; i < k; i++)
+                {
+                    float randomAngle = Random.Range(0.0f, 2.0f*Mathf.PI);
+                    float randomLength = Random.Range(r, 2.0f*r);
+                    float offX = Mathf.Cos(randomAngle) * randomLength;
+                    float offZ = Mathf.Sin(randomAngle) * randomLength;
+
+                    // TODO: Continue
+                }
+
+            }
+
+
+            // Offset each tree randomly inside it's cell to break the visible grid pattern
+            /*
+            float gridOffset = 0.5f * SettingsManager.Instance.ChunkSettings.gridCellSize;
+            float randomOffsetX = Random.Range(0.0f, gridOffset);
+            float randomOffsetZ = Random.Range(0.0f, gridOffset);
+            */
+
+
+            // ... Do plausability check for each plant separately ... //
+            // TODO
             // Do not place vegetation below water level
             if (y < SettingsManager.Instance.MeshSettings.waterLevel) return;
-            if (y > 2.0f) return; // TODO: DEBUG REMOVE!
+            // Do not place vegetation above certain height
+            // TODO
 
-            // Decide if vegetation is placed based on the distribtution probability
-            // TODO: Run random experiment with expected value and standard deviation and do poisson disk sampling afterwards
-            if (Random.value <= 0.5f)
-            {
-                // Offset each tree randomly inside it's cell to break the visible grid pattern
-                float gridOffset = 0.5f * SettingsManager.Instance.ChunkSettings.gridCellSize;
-                float randomOffsetX = Random.Range(0.0f, gridOffset);
-                float randomOffsetZ = Random.Range(0.0f, gridOffset);
-
-                // Spawn tree
-                // TODO: Spawn multiple trees using poisson disc sampling
-                InstantiateTree(new Vector3(centerX + randomOffsetX, y, centerZ + randomOffsetZ));
-            }
+                
+            // InstantiateTree(new Vector3(centerX + randomOffsetX, y, centerZ + randomOffsetZ));
         }
 
         private void InstantiateTree(Vector3 pos)
